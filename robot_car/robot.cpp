cc8342c9a03bd2bb061591671a1987b3b442a976
tykosa <kosakowski.t@husky.neu.edu>
2021-08-24 00:03:06 -0400

    Add implementation of Drive to Robot
    Includes a full implementation of the Drive method, and the inclusion
    of kStop to the DriveDirection enum

--- robot_car/robot.cpp
+++ robot_car/robot.cpp
@@ -2,38 +2,90 @@
 
 // Constructor
 Robot::Robot() {
-  return;
+    return;
 }
 
 
 // Destructor
 Robot::~Robot() {
-  return;
+    return;
 }
 
 void Robot::Setup() {
-  return;
+    return;
 }
 
 void Robot::Drive(
-    DriveDirection dir,
-    int duration,
-    int left_motor_speed,
-    int right_motor_speed) {
-  
-  return;
+        DriveDirection dir,
+        int duration,
+        int left_motor_speed,
+        int right_motor_speed) {
+    switch (dir) {
+        case kForward:
+            digitalWrite(kRightMotorDir1, HIGH);
+            digitalWrite(kRightMotorDir2, LOW);
+            digitalWrite(kLeftMotorDir1, HIGH);
+            digitalWrite(kLeftMotorDir2, LOW);
+            analogWrite(kMotorSpeedLeft, left_motor_speed);
+            analogWrite(kMotorSpeedRight, right_motor_speed);
+            delay(duration);
+            break;
+        case kBackward:
+            digitalWrite(kRightMotorDir1, LOW);
+            digitalWrite(kRightMotorDir2, HIGH);
+            digitalWrite(kLeftMotorDir1, LOW);
+            digitalWrite(kLeftMotorDir2, HIGH);
+            analogWrite(kMotorSpeedLeft, left_motor_speed);
+            analogWrite(kMotorSpeedRight, right_motor_speed);
+            delay(duration);
+            break;
+        case kLeft:
+            digitalWrite(kRightMotorDir1, LOW);
+            digitalWrite(kRightMotorDir2, HIGH);
+            digitalWrite(kLeftMotorDir1, HIGH);
+            digitalWrite(kLeftMotorDir2, LOW);
+            analogWrite(kMotorSpeedLeft, left_motor_speed);
+            analogWrite(kMotorSpeedRight, right_motor_speed);
+            delay(duration);
+            break;
+        case kRight:
+            digitalWrite(kRightMotorDir1, HIGH);
+            digitalWrite(kRightMotorDir2, LOW);
+            digitalWrite(kLeftMotorDir1, LOW);
+            digitalWrite(kLeftMotorDir2, HIGH);
+            analogWrite(kMotorSpeedLeft, left_motor_speed);
+            analogWrite(kMotorSpeedRight, right_motor_speed);
+            delay(duration);
+            break;
+        case kStop:
+            digitalWrite(kRightMotorDir1, LOW);
+            digitalWrite(kRightMotorDir2, LOW);
+            digitalWrite(kLeftMotorDir1, LOW);
+            digitalWrite(kLeftMotorDir2, LOW);
+            analogWrite(kMotorSpeedLeft, 0);
+            analogWrite(kMotorSpeedRight, 0);
+            break;
+        default:
+            digitalWrite(kRightMotorDir1, LOW);
+            digitalWrite(kRightMotorDir2, LOW);
+            digitalWrite(kLeftMotorDir1, LOW);
+            digitalWrite(kLeftMotorDir2, LOW);
+            delay(duration);
+            break;
+    }
+    return;
 }
 
 int Robot::ReadUltrasonic() {
   
-  return;
+    return;
 }
 
 void Robot::UpdateLineSensorValues() {
 
-  return;
+    return;
 }
 
 bool * Robot::GetIrSensorValues() {
-  return ir_sensor_values_;
+    return ir_sensor_values_;
 }
--- robot_car/robot.h
+++ robot_car/robot.h
@@ -21,77 +21,78 @@ enum DriveDirection {
   kForward = 0,
   kBackward = 1,
   kLeft = 2,
-  kRight = 3
+  kRight = 3,
+  kStop = 4
 };
 
 class Robot {
-  private:
-    // Boolean Array holding the data read from the line sensors
-    bool ir_sensor_values_[5];
+    private:
+        // Boolean Array holding the data read from the line sensors
+        bool ir_sensor_values_[5];
 
-    // ir_sensor_0 - 4 are the 5 ir line following sensors
-    // these are each assigned a pin number here
-    const int kIrSensor0 = A0;
-    const int kIrSensor1 = A1;
-    const int kIrSensor2 = A2;
-    const int kIrSensor3 = A3;
-    const int kIrSensor4 = A4;
+        // ir_sensor_0 - 4 are the 5 ir line following sensors
+        // these are each assigned a pin number here
+        const int kIrSensor0 = A0;
+        const int kIrSensor1 = A1;
+        const int kIrSensor2 = A2;
+        const int kIrSensor3 = A3;
+        const int kIrSensor4 = A4;
 
-    // Speed control pins for each side of the robot
-    const int kMotorSpeedRight = 3;
-    const int kMotorSpeedLeft = 6;
-    
-    // Direction control pins for each of the motors
-    const int kRightMotorDir1 = 11;
-    const int kRightMotorDir2 = 12;
-    const int kLeftMotorDir1 = 8;
-    const int kLeftMotorDir2 = 7;
+        // Speed control pins for each side of the robot
+        const int kMotorSpeedRight = 3;
+        const int kMotorSpeedLeft = 6;
+        
+        // Direction control pins for each of the motors
+        const int kRightMotorDir1 = 11;
+        const int kRightMotorDir2 = 12;
+        const int kLeftMotorDir1 = 8;
+        const int kLeftMotorDir2 = 7;
 
-    // Ultrasonic control pins
-    const int kUltrasonicEchoPin = 2;
-    const int kUltrasonicTriggerPin = 10;
+        // Ultrasonic control pins
+        const int kUltrasonicEchoPin = 2;
+        const int kUltrasonicTriggerPin = 10;
 
-    // Servo control pins
-    const int kServoPin = 9;
+        // Servo control pins
+        const int kServoPin = 9;
 
-    // Default motor speed
-    const int kDefaultMotorSpeed = 200;
+        // Default motor speed
+        const int kDefaultMotorSpeed = 200;
 
-    // Ultrasonic distance conversion constant (result is in cm)
-    const double kUltrasonicDistanceConversion = 0.017;
+        // Ultrasonic distance conversion constant (result is in cm)
+        const double kUltrasonicDistanceConversion = 0.017;
 
-    // Ultrasonic detection distance limit (units in cm)
-    const int kUltrasonicDistanceLimit = 40;
+        // Ultrasonic detection distance limit (units in cm)
+        const int kUltrasonicDistanceLimit = 40;
 
-  public:
-    // Constructor
-    Robot();
+    public:
+        // Constructor
+        Robot();
 
-    // Destructor
-    ~Robot();
+        // Destructor
+        ~Robot();
 
-    // Setup Method to initalize all of the pin configurations
-    void Setup();
+        // Setup Method to initalize all of the pin configurations
+        void Setup();
 
-    // Robot driving control method
-    // @param dir direction to drive in
-    // @param duration how long to spend before returning from drive
-    // @param left_motor_speed how fast to run the left motors
-    // @param right_motor_speed how fast to run the right motors
-    void Drive(
-      DriveDirection dir,
-      int duration=0,
-      int left_motor_speed=kDefaultMotorSpeed,
-      int right_motor_speed=kDefaultMotorSpeed);
-    
-    // Reads the ultrasonic sensor and returns the distance to an obstacle
-    int ReadUltrasonic();
+        // Robot driving control method
+        // @param dir direction to drive in
+        // @param duration how long to spend before returning from drive
+        // @param left_motor_speed how fast to run the left motors
+        // @param right_motor_speed how fast to run the right motors
+        void Drive(
+        DriveDirection dir,
+        int duration=0,
+        int left_motor_speed=kDefaultMotorSpeed,
+        int right_motor_speed=kDefaultMotorSpeed);
+        
+        // Reads the ultrasonic sensor and returns the distance to an obstacle
+        int ReadUltrasonic();
 
-    // Reads each of the ir line sensors and stores the values
-    void UpdateLineSensorValues();
+        // Reads each of the ir line sensors and stores the values
+        void UpdateLineSensorValues();
 
-    // getter for the ir line sensors array
-    bool * GetIrSensorValues();
+        // getter for the ir line sensors array
+        bool * GetIrSensorValues();
 };
 
 
